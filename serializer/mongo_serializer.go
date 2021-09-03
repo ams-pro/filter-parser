a6814e3006d6afb9a7c863afe825fbd75919b589
Aaron Schweig <aaron.schweig@gmail.com>
2021-09-03 22:47:17 +0200

    adds eq serializer to mongodb

--- serializer/mongo_serializer.go
+++ serializer/mongo_serializer.go
@@ -18,25 +18,26 @@ func isNotEmpty(in string) bool {
 }
 
 func parseSliceFromString(in string) interface{} {
+	in = strings.TrimSpace(in)
 	slice := strings.Split(in[1:len(in)-1], ",")
 
 	if len(slice) == 0 {
 		return nil
 	}
 
-	if _, err := strconv.ParseInt(slice[0], 10, 64); err == nil {
+	if _, err := strconv.ParseInt(strings.TrimSpace(slice[0]), 10, 64); err == nil {
 		var returnValue []int64
 		for _, item := range slice {
-			j, _ := strconv.ParseInt(item, 10, 64)
+			j, _ := strconv.ParseInt(strings.TrimSpace(item), 10, 64)
 			returnValue = append(returnValue, j)
 		}
 		return returnValue
 	}
 
-	if primitive.IsValidObjectID(slice[0]) {
+	if primitive.IsValidObjectID(strings.TrimSpace(slice[0])) {
 		var returnValue []primitive.ObjectID
 		for _, item := range slice {
-			oid, _ := primitive.ObjectIDFromHex(item)
+			oid, _ := primitive.ObjectIDFromHex(strings.TrimSpace(item))
 			returnValue = append(returnValue, oid)
 		}
 		return returnValue
@@ -55,7 +56,24 @@ func parseMongo(filter bson.M, tree *filterparser.Node) (bson.M, error) {
 			return nil, ErrInvalidFilterValue
 		}
 		slice := parseSliceFromString(tree.Right.Token)
-		filter[tree.Left.Token] = bson.M{"$in": slice}
+		if slice != nil {
+			filter[tree.Left.Token] = bson.M{"$in": slice}
+		}
+	case "and":
+		if tree.Left == nil && tree.Right == nil {
+			return nil, ErrInvalidFilterValue
+		}
+
+		filter, _ = parseMongo(filter, tree.Left)
+		filter, _ = parseMongo(filter, tree.Right)
+	case "eq":
+		left, right := strings.TrimSpace(tree.Left.Token), strings.TrimSpace(tree.Right.Token)
+		var value interface{} = right
+		if primitive.IsValidObjectID(right) {
+			value, _ = primitive.ObjectIDFromHex(right)
+		}
+
+		filter[left] = value
 	}
 	return filter, nil
 }
